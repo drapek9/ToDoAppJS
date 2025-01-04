fe0a8ff28ca3710c71a823bb243dc893ccaef308
Šimon Drápal <simondrapal9@gmail.com>
2025-01-04 11:38:27 +0100

    načtení nazačátku z localstorage + funkce poslání do localstorage + základ struktury toDo tasku a zisk aktuálního měsíce + pokud mouse enter - ukázat čas

--- script-functions.js
+++ script-functions.js
@@ -1,3 +1,4 @@
+"use strict"
 const set_all_values_roll = (list_names, the_select) => {
     list_names.forEach((one_name) => {
         let new_option = document.createElement("option")
@@ -8,3 +9,41 @@ const set_all_values_roll = (list_names, the_select) => {
 }
 
 const set_value_color = (event) => typeof(event) !== "string" ? document.querySelector("#taskNameId").style.color = values_colors[event.target.options[event.target.selectedIndex].value] : document.querySelector("#taskNameId").style.color = values_colors[event]
+
+const saveToLocalStorage = (where_name, content) => {
+    localStorage.setItem(where_name, JSON.stringify(content))
+}
+
+const createHTMLStructure = (oneToDo) => {
+    let divTogether = document.createElement("div")
+    let textName = document.createElement("p")
+    let checkBox = document.createElement("input")
+    let dateUpdate = document.createElement("p")
+
+    textName.textContent = oneToDo.toDoName
+
+    let updateDate = new Date(oneToDo.updateTime)
+    dateUpdate.innerHTML = `<span class="timeUpdateVisual" name="timeUpdateVisualName">${updateDate.toTimeString().split(" ")[0]}</span>  ${updateDate.getDate().toString().padStart(2, "0")}.${(updateDate.getMonth()+1).toString().padStart(2, "0")}.${updateDate.getFullYear()}`
+
+    checkBox.type = "checkbox"
+    textName.style.color = values_colors[oneToDo.value]
+
+    textName.classList.add("taskNameVisual")
+    divTogether.classList.add("divTogetherVisual")
+    checkBox.classList.add("checkBoxVisual")
+    dateUpdate.classList.add("dateUpdateVisual")
+
+    divTogether.appendChild(checkBox)
+    divTogether.appendChild(textName)
+    divTogether.appendChild(dateUpdate)
+
+    divTogether.addEventListener("mouseenter", (event) => {
+        event.target.querySelector("span").style.display = "inline-block"
+    })
+
+    divTogether.addEventListener("mouseleave", (event) => {
+        event.target.querySelector("span").style.display = "none"
+    })
+
+    return divTogether
+}
--- script.js
+++ script.js
@@ -7,10 +7,18 @@ let values_colors = {
     5: "#ff9100" // orange
 }
 
+const getFromLocalStorage = (where_name) => {
+    return JSON.parse(localStorage.getItem(where_name))
+}
 
+let allToDos = getFromLocalStorage("toDos")
+if (!allToDos){
+    allToDos = []
+}
 set_all_values_roll(Object.keys(values_colors),document.querySelector("#value_todo_id"))
 const to_do_element_value = document.querySelector("#value_todo_id")
 set_value_color(to_do_element_value.options[to_do_element_value.selectedIndex].value)
+
 
 document.querySelector("#add_layout").addEventListener("submit", (event) => {
     event.preventDefault()
@@ -18,7 +26,23 @@ document.querySelector("#add_layout").addEventListener("submit", (event) => {
     if (task_name){
         // let task_color = values_colors[event.target.elements.value_todo.value]
         console.log(window.getComputedStyle(event.target.elements.taskName).color)
+        allToDos.push(
+            {
+                toDoName: task_name,
+                value: event.target.elements.value_todo.value,
+                updateTime: new Date().toISOString()
+            }
+        )
+        saveToLocalStorage("toDos", allToDos)
+
+        event.target.elements.taskName.value = ""
     }
+})
+
+let contentDiv = document.querySelector("#tasks_content")
+allToDos.forEach((oneToDo) => {
+    let structure = createHTMLStructure(oneToDo)
+    contentDiv.appendChild(structure)
 })
 
 to_do_element_value.addEventListener("change", (event) => set_value_color(event))
