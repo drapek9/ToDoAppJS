74ec2450720968d95c43e5d26c5aa1ef9f8e34c5
Šimon Drápal <simondrapal9@gmail.com>
2025-01-04 03:24:15 +0100

    nastavení barvy v inputu po změně value + nastavení barvy hned na začátku

--- script-functions.js
+++ script-functions.js
@@ -6,3 +6,5 @@ const set_all_values_roll = (list_names, the_select) => {
         the_select.appendChild(new_option)
     })
 }
+
+const set_value_color = (event) => typeof(event) !== "string" ? document.querySelector("#taskNameId").style.color = values_colors[event.target.options[event.target.selectedIndex].value] : document.querySelector("#taskNameId").style.color = values_colors[event]
--- script.js
+++ script.js
@@ -7,14 +7,18 @@ let values_colors = {
     5: "#ff9100" // orange
 }
 
+
 set_all_values_roll(Object.keys(values_colors),document.querySelector("#value_todo_id"))
+const to_do_element_value = document.querySelector("#value_todo_id")
+set_value_color(to_do_element_value.options[to_do_element_value.selectedIndex].value)
 
-
-document.querySelector("#add_layout").addEventListener("submit", function(event){
+document.querySelector("#add_layout").addEventListener("submit", (event) => {
     event.preventDefault()
     let task_name = event.target.elements.taskName.value
     if (task_name){
-        let task_color = values_colors[event.target.elements.value_todo.value]
-        console.log(task_color)
+        // let task_color = values_colors[event.target.elements.value_todo.value]
+        console.log(window.getComputedStyle(event.target.elements.taskName).color)
     }
 })
+
+to_do_element_value.addEventListener("change", (event) => set_value_color(event))
